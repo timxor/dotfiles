24f58a6fdc3917c4d9a67eb0b2109bee4f86307a
Tim Siwula <tim@acceptance.ai>
2020-04-07 05:55:51 -0400

    :bowtie: [auto generated] via tim's deploy_script.sh

---
+++ a_new_mac_install.sh
@@ -0,0 +1,35 @@
+
+
+# vagrant 
+
+
+# virtual box
+brew cask install virtualbox
+
+
+# vagrant
+brew cask install vagrant
+brew cask install vagrant-manager
+
+vagrant box add hashicorp/precise64
+
+
+vagrant init precise64
+
+mkdir ~/vagrant
+cd ~/vagrant
+vagrant box list
+vagrant init hashicorp/percise64
+vagrant up
+
+vagrant ssh
+pwd
+whoami
+ls -la
+
+vagrant halt
+
+
+brew install terraform
+
+
