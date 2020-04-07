53dac5d3ecd0cb8de797507c3a1106f896d11e87
Tim Siwula <tim@acceptance.ai>
2020-04-07 04:08:17 -0400

    :bowtie: [auto generated] via tim's deploy_script.sh

--- deploy_script.sh
+++ deploy_script.sh
@@ -1,15 +1,25 @@
 #!bin/bash
+#
+# file:           deploy_script.sh
+# date:           04/07/2020
+# author:         tim siwula <tim@acceptance.ai>
+#
+# mac os setup:
+#
+#      - cp deploy_script.sh /usr/local/bin/
+#      - nano ~/.zshrc
+#      - add this: alias deploy="bash /usr/local/bin/deploy_script.sh"
+#      - source ~/.zshrc
+#      - chmod +x /usr/local/bin/deploy_script.sh
+#      - deploy
+#
 
-# cp deploy_script.sh /usr/local/bin/
-# alias deploy="bash /usr/local/bin/deploy_script.sh"
-# chmod +x /usr/local/bin/deploy_script.sh
-# bash /usr/local/bin/deploy_script.sh
-
-echo "-----> starting /usr/local/bin/deploy_script.sh ---->"
+echo ".\n ..\n   running deploy script\n   ..\n  ."
 git status
+git pull
 git add .
 git status
-git commit -m ":bowtie: [auto generated] via tim's deploy_script.sh"
+git commit -m ":smile: tim_bot :smile: via tim's deploy_script.sh"
 git push
 git status
 echo "<----- done <----"
