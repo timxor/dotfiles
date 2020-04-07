8725e7b5f7299fb668115e0e8e0242004b9ee1cb
Tim Siwula <tim@acceptance.ai>
2020-04-07 09:10:36 -0400

    :bowtie: [auto generated] via tim's deploy_script.sh

--- install_flutter.sh
+++ install_flutter.sh
@@ -6,3 +6,18 @@ flutter config --android-sdk /Users/tim/Library/Android/sdk
 echo $ANDROID_HOME
 flutter doctor --android-licenses
 
+flutter precache
+sudo xcode-select --switch /Applications/Xcode.app/Contents/Developer
+sudo xcodebuild -runFirstLaunch
+open -a Simulator
+flutter create my_app
+cd my_app
+flutter run
+sudo gem install cocoapods
+pod setup
+
+
+flutter clean
+flutter devices
+
+flutter run
