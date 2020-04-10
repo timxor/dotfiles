37d6503e6c1514a3340c51b0d11419fb04d63f97
Tim Siwula <tim@acceptance.ai>
2020-04-10 01:45:02 -0400

    :smile: tim_bot :smile: via tim's deploy_script.sh

---
+++ mac_new_setup.sh
@@ -0,0 +1,28 @@
+#!/bin/zsh
+
+
+# /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+
+
+# brew install wget
+# brew cask install firefox
+
+
+echo ""
+echo "running mac_new_setup.sh"
+echo ""
+
+
+echo ""
+echo "installing macvim "
+echo ""
+
+#brew install vim
+brew install macvim
+
+source ~/.bash_profile
+source ~/.zshrc
+
+echo ""
+echo "done. "
+echo ""
